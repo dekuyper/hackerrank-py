17dc1424a0f87c651d662ddf8f97f3758d4b1338
Nicu Oprisan <nicu.oprisan@gmail.com>
2017-03-27 12:54:10 +0300

    changed time-conversion to work in more cases; added sublime project config

--- easy/time-conversion.py
+++ easy/time-conversion.py
@@ -1,9 +1,11 @@
-time = '04:59:59AM'
+time = '12:45:54PM'
 
 def time_conversion(time):
 	time = time.split(':')
-	time[0] = (str(int(time[0]) + 12), time[0])[time[-2:] == 'AM'] 
-	time[0] = (time[0], '00')[time[0] == '24']
+	time[0] = (str(int(time[0]) + 12), time[0])[time[-1][-2:] == 'AM']
+	print(time)
+	time[0] = (time[0], '00')[time[0] == '24' or (time[0] == '12' and time[-1][-2:] == 'AM')]
 	print ':'.join(time)[:-2]
+
 
 time_conversion(time)
